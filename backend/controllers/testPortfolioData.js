06e79975e58931a3229e6b89048d14d3fab4da15
jdwendler <60332035+jdwendler@users.noreply.github.com>
2022-10-11 18:45:33 -0500

    Created file for loading test data into MongoDB

---
+++ backend/controllers/testPortfolioData.js
@@ -0,0 +1,56 @@
+    {
+        userId: 1,
+        portId: 1,
+        stockName: 'TSLA',
+        numOfUnits: 100,
+        initCost: 312.34
+    },
+    {
+        userId: 1,
+        portId: 1,
+        stockName: 'AAPL',
+        numOfUnits: 150,
+        initCost: 163.86
+    },
+    {
+        userId: 1,
+        portId: 1,
+        stockName: 'MSFT',
+        numOfUnits: 125,
+        initCost: 284.68
+    },
+    {
+        userId: 1,
+        portId: 1,
+        stockName: 'NVDA',
+        numOfUnits: 200,
+        initCost: 218.67
+    },
+    {
+        userId: 2,
+        portId: 1,
+        stockName: 'TSLA',
+        numOfUnits: 200,
+        initCost: 300.15
+    },
+    {
+        userId: 2,
+        portId: 1,
+        stockName: 'F',
+        numOfUnits: 50,
+        initCost: 12.45
+    },
+    {
+        userId: 2,
+        portId: 1,
+        stockName: 'MSFT',
+        numOfUnits: 150,
+        initCost: 250.00
+    },
+    {
+        userId: 2,
+        portId: 1,
+        stockName: 'TWTR',
+        numOfUnits: 75,
+        initCost: 60.00
+    }
