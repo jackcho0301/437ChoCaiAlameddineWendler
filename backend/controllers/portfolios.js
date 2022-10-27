d63cf6a428ab0c7a087809ba717e01cfa4e61bbe
jdwendler <60332035+jdwendler@users.noreply.github.com>
2022-10-27 17:25:27 -0500

    Allow creation of up to four portfolios

--- backend/controllers/portfolios.js
+++ backend/controllers/portfolios.js
@@ -11,33 +11,42 @@ const {BadRequestError, UnauthenticatedError} = require('../errors');
 const createPortfolio = async (req, res) => {
     const startingMoney = 10000
     const userID = req.user.userId;
-    // Default for now
-    const portID = 1
+    let portID = 1
 
     // First verify that the user has not already created max portfolios
     const portfolioInfo = await Portfolio.find()
-    const portInfo = retrievePortInfoKernel(userID, portID, portfolioInfo)
-    if (portInfo.length > 0) {
-        // TEST CODE - Check for the presence of a "dollars" portfolio.
-        // This is being used only to correct past MongoDB entries
-        // Note that this only checks for one portfolio. It needs to check for 4.
-        const dollarsItem = portInfo.filter((item) => item.stockName === "dollars")
-        if (dollarsItem.length > 0){
-            res.status(400).json({success:false, msg:'You are already at max number of portfolios.'})
+    let portInfo;
+    let portfolio;
+    while (portID <= 4) {
+        portInfo = retrievePortInfoKernel(userID, portID, portfolioInfo)
+        if (portInfo.length > 0) {
+            // TEST CODE - Check for the presence of a "dollars" portfolio.
+            // This is being used only to correct past MongoDB entries
+            const dollarsItem = portInfo.filter((item) => item.stockName === "dollars")
+            if (dollarsItem.length == 0){
+                // Make a new portfolio entry for "dollars".
+                portItem = ({userId:userID, portId:portID, stockName:"dollars", numOfUnits:startingMoney, initCost:1})
+                portfolio = await Portfolio.create(portItem);
+                res.status(StatusCodes.CREATED).json({portfolio});
+                break;
+            }
         }
-        else{
+        else
+        {
             // Make a new portfolio entry for "dollars".
             portItem = ({userId:userID, portId:portID, stockName:"dollars", numOfUnits:startingMoney, initCost:1})
-            const portfolio = await Portfolio.create(portItem);
+            portfolio = await Portfolio.create(portItem);
             res.status(StatusCodes.CREATED).json({portfolio});
+            break;
         }
+        portID += 1
     }
-    else
-    {
-        // Make a new portfolio entry for "dollars".
-        portItem = ({userId:userID, portId:portID, stockName:"dollars", numOfUnits:startingMoney, initCost:1})
-        const portfolio = await Portfolio.create(portItem);
+
+    if (portID <= 4){
         res.status(StatusCodes.CREATED).json({portfolio});
+    }
+    else {
+        res.status(400).json({msg: "You are already at the maximum number of portfolios."});
     }
 }
 
