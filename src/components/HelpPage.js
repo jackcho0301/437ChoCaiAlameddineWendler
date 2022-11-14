4a9f7871f84c7aa277da9a7b67cab53512c4b801
Christina Cai <christinacai@wustl.edu>
2022-11-14 17:56:08 -0600

    home and help page edits

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -20,23 +20,25 @@ export default function HelpPage(props) {
                                 Choose "My Ranking" to see your past performance history and your current rank and title this quarter.
                                 Choose "Top Earners" to see the current leaderboard, search for different users scores, and see if you made it.
                                 Choose “FAQs” to see common questions and answers from users and whenever you need help.
-                                Choose your profile icon to edit your current information.</p>
+                                Choose your profile icon to see your profile and edit your current information.</p>
                         </Panel>
                         <Panel className='description-header' header="How is the score determined?" key="2">
                             <p>Our application calculates your score based on the ROI of your portfolio.</p>
                         </Panel>
                         <Panel className='description-header' header="What do the titles mean?" key="3">
-                            <p>Insert answer here</p>
+                            <p>At the end of a season if your ranking is high enough on the leaderboard you will be awarded a corresponding title for your rank. The current #1 title is 'Global Elite'. This 
+                                title can be displayed on your "My Ranking" page.
+                            </p>
                         </Panel>
                         <Panel className='description-header' header="Does this game use real money?" key="4">
                             <p>No! Our application allows you to use fake, virtual money to simulate a stock trading environment with no cost to you.</p>
                         </Panel>
                     </Collapse>
                 </div>
-                <div className='support'>
+                {/* <div className='support'>
                     <h3>Contact for Questions</h3>
                     <p></p>
-                </div>
+                </div> */}
             </div>
         </div>
     )
