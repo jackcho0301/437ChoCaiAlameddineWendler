c3eb14d07a26080f9d65d09e8b561d095e7d193b
jdwendler <60332035+jdwendler@users.noreply.github.com>
2022-11-21 17:07:06 -0600

    Update with new methods for preferred portfolio

--- backend/routes/portfolios.js
+++ backend/routes/portfolios.js
@@ -2,9 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-const {getRankings, getPortfolio,updatePortfolio,sellPortfolioItem, createPortfolio} = require('../controllers/portfolios')
+const {getRankings, getPortfolio, updatePortfolio,sellPortfolioItem, createPortfolio, changePreferredPortfolio, getPreferredPortfolio} = require('../controllers/portfolios')
 
 router.route('/').get(getRankings).post(createPortfolio)
 router.route('/:id').get(getPortfolio).delete(sellPortfolioItem).patch(updatePortfolio)
+router.route('/choice/portnumber').get(getPreferredPortfolio).post(changePreferredPortfolio)
 
 module.exports = router
