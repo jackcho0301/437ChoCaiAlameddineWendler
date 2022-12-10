88c7fa6e1e4334171669f64cc6f95e0a6f14ca42
jdwendler <60332035+jdwendler@users.noreply.github.com>
2022-12-09 18:49:04 -0600

    Update routes with new endpoint

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -2,9 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-const {getUserGroupMemberships, getGroupMembers, putGroupMembership} = require('../controllers/groups')
+const {getUserGroupMemberships, getGroupMembers, putGroupMembership, allGroups} = require('../controllers/groups')
 
 router.route('/').get(getUserGroupMemberships).post(putGroupMembership)
 router.route('/:group').get(getGroupMembers)
+router.route('/groups/all').get(allGroups)
 
 module.exports = router
